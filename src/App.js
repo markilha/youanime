39cb885963088c45574d5c627a49835f802fe4b9
Marco Antonio Monteiro de Almeida <malmeida@itesp.sp>
2022-03-24 16:33:51 -0300

    Emprementado dark

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, { useState } from 'react';
 import {ThemeProvider,createTheme,makeStyles } from '@material-ui/core';
 import './App.css';
 import Home from './components/Home'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
  
   const theme = createTheme({
     spacing:4,
    palette:{
+    type: darkMode ? 'dark' : 'light',
      primary:{
        main: '#f44336'
      },
      secondary:{
        main:'#3f51b5'
-     }
+     },
+     background: {
+      default: darkMode ? '#232323' : '#FFF',
+      dark: darkMode ? '#181818' : '#f4f6f8',
+      paper: darkMode ? '#232323' : '#FFF',
+    },
    },
   });
 
 
   return (
     <ThemeProvider theme={theme}>
-     <Home/>
+     <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
     </ThemeProvider>
   
   );
